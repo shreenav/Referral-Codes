298e0418c00dc93f3e0a47252d8a85e6ca63bc7a
Shreenav Khandelwal <shreenavkhandelwalsk200@gmail.com>
2026-04-01 23:48:33 +0530

    Update Next.js config for better Vercel compatibility

--- next.config.js
+++ next.config.js
@@ -3,6 +3,9 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   pageExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  experimental: {
+    isrMemoryCacheSize: 0,
+  },
 }
 
 module.exports = nextConfig
